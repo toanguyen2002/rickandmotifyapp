c3665bd33485d056300c2df2add9e4476b1c268c
toanguyen2002 <toanguyen200220@gmail.com>
2023-08-13 14:42:35 +0700

    NOI QUY CODe

--- Therickandmotifyapp/Controller/core/RM_ChacraterViewController.swift
+++ Therickandmotifyapp/Controller/core/RM_ChacraterViewController.swift
@@ -8,15 +8,45 @@
 import UIKit
 
 class RM_ChacraterViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        title = "Chacrater"
+        
+        vỉewChacrater();
         // Do any additional setup after loading the view.
     }
     
-
+    public func vỉewChacrater(){
+//        title = "Chacraters"
+        
+        
+        let label1 = UILabel(frame: CGRect(x: 0, y: 100, width: 300, height: 100))
+        label1.text = "Nhập số thứ nhất"
+        label1.textAlignment = .center
+        label1.textColor = .red
+        
+        let text1 = UITextView(frame: CGRect(x: 300, y: 100, width: 300, height: label1.frame.size.height))
+        text1.text = "number 1"
+        text1.layer.borderColor = .init(red: 199, green: 22, blue: 12, alpha: 43)
+        text1.layer.borderWidth = 5
+        
+        
+        
+        
+        let label2 = UILabel(frame: CGRect(x: 0, y: 200, width: 300, height: 100))
+        label2.text = "Nhập số thứ Hai"
+        label2.textAlignment = .center
+        label2.textColor = .red
+        
+        let text2 = UITextView(frame: CGRect(x: 10, y: 200, width: 50, height: 100))
+      
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(text1)
+        view.addSubview(text2)
+        
+    }
     /*
     // MARK: - Navigation
 
